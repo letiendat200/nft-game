ee9eb0446440800475a1800fa25d791b2216e2e0
Nguyen Xuan Hanh <67793801+hanhxuan1804@users.noreply.github.com>
2023-06-30 22:37:03 +0700

    add page

--- fe/src/pages/welcome/WelcomeScreen.jsx
+++ fe/src/pages/welcome/WelcomeScreen.jsx
@@ -1,14 +1,40 @@
+import "../bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
 function WelcomeScreen() {
-  return (
-    <>
-      <div
-        style={{
-          backgroundImage: `url("https://realpython.com/python-rock-paper-scissors/")`,
-        }}
-      >
-        Wellcome to our GAME
-      </div>
-    </>
-  );
+       const defaultTheme = createTheme();
+return (
+       <>
+       <ThemeProvider theme={defaultTheme}>
+         <Box
+           sx={{
+             display: 'flex',
+             flexDirection: 'column',
+             minHeight: '100vh',
+           }}
+         >
+           <CssBaseline />
+           <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+             <Typography variant="h2" component="h1" gutterBottom>
+             Please sign in to Metamask first
+             </Typography>
+             <Typography variant="h5" component="h2" gutterBottom>
+               {'This web will only works if you have Metamask install on your browser'}
+               
+             </Typography>
+           
+             
+             <Typography variant="body1"></Typography>
+           </Container>
+           </Box>
+        </ThemeProvider>
+       
+       </>
+ )
 }
-export default WelcomeScreen;
+ export default WelcomeScreen;
